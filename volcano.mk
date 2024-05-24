c89b68247333520974baf3972c9f5f4030d4d8ae
Divyanand Rangu <quic_drangu@quicinc.com>
2024-05-24 03:08:20 -0700

    Volcano: Increase Dalvik Heap params
    This reverts commit 628a8b72c912421083f1aff051c093fe71604e72.
    
    Change-Id: Ie0f207765fdc4b94d151068fbfac738a1be8181a

--- volcano.mk
+++ volcano.mk
@@ -249,15 +249,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, device/qcom/vendor-common/common64.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
-#Product property overrides to configure the Dalvik heap
-PRODUCT_PROPERTY_OVERRIDES  += \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapsize=256m \
-    dalvik.vm.heapgrowthlimit=128m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m
-
 # Below perf props should be part of vendor/build.prop
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
