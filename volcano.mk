4cb8200c03dbea6a5016e97896a08cd03fb86357
changyuan1.zhou <changyuan.zhou@t2mobile.com>
2025-04-15 14:51:59 +0800

    [FPS-2008] ESWIN TP firmware update to v2450
    * Root Cause
      TP firmware update to v2450
    * Solution
      TP firmware update to v2450
    * Test Steps
      can touch
    * Test Result
      pass
    
    Change-Id: I94f005fd48dd035b311acb2d4ca721f4deb823fe

--- volcano.mk
+++ volcano.mk
@@ -544,7 +544,7 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.fuse.passthrough.enable=true
 
 #tp firmware
 PRODUCT_COPY_FILES += \
-    device/qcom/volcano/fps/tpfirmware/EPH86XX_fw_v2448.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/EPH86XX_fw.bin\
+    device/qcom/volcano/fps/tpfirmware/EPH86XX_fw_v2450.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/EPH86XX_fw.bin\
 # ODM ueventd.rc
 # - only for use with VM support right now
 ifeq ($(TARGET_ENABLE_VM_SUPPORT),true)
